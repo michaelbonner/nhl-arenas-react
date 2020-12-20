a3b4726c2133ff7fa2c48bff7164440a36a120dd
Michael Bonner <michaelwbonner@gmail.com>
2020-12-20 11:27:05 -0700

    use environment var for maps key

--- src/components/MyMapComponent.js
+++ src/components/MyMapComponent.js
@@ -10,8 +10,7 @@ import mapStyles from "../data/map-styles";
 
 const MyMapComponent = compose(
   withProps({
-    googleMapURL:
-      "https://maps.googleapis.com/maps/api/js?key=AIzaSyAYGe5aS07NFhhITkwac8OQKazamMW3SNU&v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_MAPS_KEY}&v=3.exp&libraries=geometry,drawing,places`,
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `800px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
