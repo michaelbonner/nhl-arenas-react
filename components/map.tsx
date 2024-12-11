c9e88048cf64ff28d1344011d17da0f9892c2576
Michael Bonner <michaelwbonner@gmail.com>
2024-12-10 17:33:51 -0700

    update bg colors

--- components/map.tsx
+++ components/map.tsx
@@ -9,13 +9,14 @@ const MapComponent = ({ markers }: Props) => {
   return (
     <APIProvider apiKey={process.env.NEXT_PUBLIC_MAPS_KEY || ""}>
       <Map
-        className="aspect-square lg:aspect-video"
+        className="aspect-square lg:aspect-video bg-[#374151]"
         style={{ width: "100vw", height: "100vh" }}
         defaultCenter={{ lat: 38.5223955, lng: -98.6003791 }}
         defaultZoom={4}
         gestureHandling={"greedy"}
         disableDefaultUI={true}
         mapId="57cb3890fb527662"
+        backgroundColor={"#374151"}
       >
         {markers &&
           markers.length > 0 &&
