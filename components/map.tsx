a415205540e819315a904f4c569a3f3efa5fff57
Michael Bonner <mike@bootpackdigital.com>
2025-07-31 09:36:50 -0600

    round the map

--- components/map.tsx
+++ components/map.tsx
@@ -10,7 +10,7 @@ const MapComponent = ({ markers }: Props) => {
   return (
     <APIProvider apiKey={process.env.NEXT_PUBLIC_MAPS_KEY || ""}>
       <Map
-        className="aspect-square lg:aspect-video bg-[#374151]"
+        className="aspect-square lg:aspect-video bg-[#374151] rounded-t overflow-hidden"
         style={{ width: "100vw", height: "100vh" }}
         defaultCenter={{ lat: 38.5223955, lng: -98.6003791 }}
         defaultZoom={4}
