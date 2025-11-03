0cb3d560c4a6392200ab3f51f876969cf2f2ab0e
Michael Bonner <michaelwbonner@gmail.com>
2025-11-03 12:14:17 -0700

    Update pin styles

--- components/map.tsx
+++ components/map.tsx
@@ -10,14 +10,17 @@ const MapComponent = ({ markers }: Props) => {
   return (
     <APIProvider apiKey={process.env.NEXT_PUBLIC_MAPS_KEY || ""}>
       <Map
+        backgroundColor={"#374151"}
         className="aspect-square lg:aspect-video bg-[#374151] rounded-t overflow-hidden"
-        style={{ width: "100vw", height: "100vh" }}
+        clickableIcons={false}
         defaultCenter={{ lat: 38.5223955, lng: -98.6003791 }}
         defaultZoom={4}
-        gestureHandling={"greedy"}
         disableDefaultUI={true}
+        gestureHandling={"greedy"}
         mapId="57cb3890fb527662"
-        backgroundColor={"#374151"}
+        maxZoom={10}
+        minZoom={4}
+        style={{ width: "100vw", height: "100vh" }}
       >
         {markers &&
           markers.length > 0 &&
@@ -27,8 +30,9 @@ const MapComponent = ({ markers }: Props) => {
                 key={marker.name}
                 position={{ lat: marker.lat, lng: marker.lng }}
               >
-                <div>
+                <div className="rounded-2xl bg-gray-100/90 size-10 p-1 shadow-sm group flex items-center justify-center border border-gray-200">
                   <Image
+                    className="max-w-6 max-h-6 size-full object-contain object-center group-hover:scale-110 transition-all duration-300"
                     src={marker.icon.src}
                     alt={marker.name}
                     width={marker.icon.width}
